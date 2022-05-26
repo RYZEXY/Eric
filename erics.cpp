1aebfdd4ebb8aea159deecd54fcbbeb0c95057b8
RYZEXY <76236234+RYZEXY@users.noreply.github.com>
2022-05-25 22:15:31 -0400

    Add files via upload

---
+++ erics.cpp
@@ -0,0 +1,13 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main ()
+{
+  while (true){
+      cout << "Eric" << '\n';
+      cout << "Yes? Which one?" << '\n';
+    }
+  return 0;
+}
